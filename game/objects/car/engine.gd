3da0bbc871901e8f4b7a2df8c81bd169e8f44daa
flurick <the3drick@gmail.com>
2018-12-31 16:05:38 +0100

    Add car velocity and a silly dial

--- game/objects/car/engine.gd
+++ game/objects/car/engine.gd
@@ -2,18 +2,20 @@ extends KinematicBody
 
 var gravity = Vector3.DOWN * 10
 var velocity = Vector3.ZERO
-var accelerate = 0
+var acceleration_input = 0
+var acceleration_multi = 2
 var steer = 0
 var turning_radius = 3
 var engine_power = 10
 var forward = Vector3.FORWARD	
+var friction = 0.1
+
 		
-				
 func _process(delta):
 	
 	if Input.is_action_just_pressed("reset"):
 		transform.origin = Vector3.ZERO
-		
+	
 	if Input.is_action_just_pressed("look back"):
 		get_tree().set_input_as_handled() 
 		find_node("Driver POV backwards").current = true
@@ -24,8 +26,14 @@ func _process(delta):
 	rotate_y(deg2rad(steer*turning_radius))
 	
 	forward = transform.basis.z.normalized()
-	accelerate = (Input.get_action_strength("accelerate")-Input.get_action_strength("reverse")) * engine_power
-	velocity = move_and_slide( forward*accelerate, Vector3.UP)
+	acceleration_input = (Input.get_action_strength("accelerate")-Input.get_action_strength("reverse")) * engine_power
+	velocity += forward * acceleration_input * acceleration_multi
+	
+	if velocity.length() != 0:
+		velocity -= Vector3.ONE * friction
+	velocity = move_and_slide( velocity, Vector3.UP)
+	
+	find_node("dial").rotation.z = deg2rad(velocity.z*0.1)
 	
 	
 	
